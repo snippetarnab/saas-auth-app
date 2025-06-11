30280482f5737dc5aae4e2b54e474bee17ff39ae
snippetarnab <arnabvivo2020@gmail.com>
2025-06-11 11:11:49 +0530

    create own middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,47 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware, clerkClient } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const publicRoutes = ["/", "/api/webhook/register", "/sign-in", "/sign-up"];
+
+export default authMiddleware({
+  publicRoutes,
+
+  async afterAuth(auth, req) {
+    //handle the unauth user trying to access a protected route
+    if (!auth.userId && !publicRoutes.includes(req.nextUrl.pathname)) {
+      return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+
+    if (auth.userId) {
+      try {
+        const user = await clerkClient.users.getUser(auth.userId);
+        const role = user.publicMetadata.role as string | undefined;
+  
+        //admin role redirection
+  
+        if (role === "admin" && req.nextUrl.pathname === "/dashboard") {
+          return NextResponse.redirect(new URL("/admin/dashboard", req.url));
+        }
+  
+        //prevent non-admin users from accessing admin routes
+        if (role !== "admin" && req.nextUrl.pathname.startsWith("/admin")) {
+          return NextResponse.redirect(new URL("/dashboard", req.url));
+        }
+  
+        //redirect authuser trying to access sign-in or sign-up
+        if (publicRoutes.includes(req.nextUrl.pathname)) {
+          return NextResponse.redirect(
+            new URL(role === "admin" ? "/admin/dashboard" : "/dashboard", req.url)
+          );
+        }
+      } catch (error) {
+        console.log("Error fetching user data:", error);
+        return NextResponse.redirect(new URL("/error", req.url));
+        
+      }
+    }
+  },
+});
 
 export const config = {
   matcher: [
